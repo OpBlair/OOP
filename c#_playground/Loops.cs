8d831636a134a7548182abaa9f80cc3ee22a51d2
Tonny Blair <jusanotherthursday@gmail.com>
2026-04-06 19:57:54 +0300

    Add files via upload

---
+++ c#_playground/Loops.cs
@@ -0,0 +1,14 @@
+namespace CSharpPractice.Loops
+{
+    public class Loops
+    {
+        public static void ShowLoops()
+        {
+            Console.WriteLine("For Loops");
+            for(int a = 0; a < 4; a++)
+            {
+                Console.WriteLine("a " + a);
+            }
+        }
+    }
+}
---
+++ c#_playground/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CSharpPractice.Loops;
+
+public class Program{
+
+    // Integers
+    static void DataTypes(){
+        int a = 10;
+        float b = 2.5f;
+        double c = 20.0;
+        char d = 'A';
+        bool isLearning = true;
+
+        Console.WriteLine("Data Types.");
+        Console.WriteLine("Integer: " + a);
+        Console.WriteLine("Float: " + b);
+        Console.WriteLine("Double: " + c);
+        Console.WriteLine("char: " + d);
+        Console.WriteLine("isLearning data types in C#: " + isLearning);
+    }
+
+    public static void Main(string[] args){
+        string name = "cyb3r_beta";
+        Console.WriteLine("Hello " + name);
+        Console.WriteLine("Which section of C# programming do you to want practice ?");
+        Console.WriteLine("1. Data Types.");
+        Console.WriteLine("2. Loops.");
+
+        Console.Write("Enter your choice: ");
+
+        if(int.TryParse(Console.ReadLine(), out int userChoice))
+        {
+            switch(userChoice){
+                case 1: DataTypes(); break;
+                case 2: Loops.ShowLoops(); break;
+                default: Console.WriteLine("No input specified!"); break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a number.");
+        }
+    }
+}
