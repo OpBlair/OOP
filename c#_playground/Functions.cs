5c9e31e4b1a7713fefaf5362651f4cab5500f19d
Tonny Blair <jusanotherthursday@gmail.com>
2026-04-08 21:40:38 +0300

    added functions

---
+++ c#_playground/Functions.cs
@@ -0,0 +1,10 @@
+namespace CSharpPractice.Functions
+{
+    public class Functions
+    {
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello Learner, How's C#(pronounced as C sharp) going?");
+        }
+    }
+}
