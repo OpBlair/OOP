d2ab09461df4c24ed93fe4c2ea62995535a8669e
Tonny Blair <jusanotherthursday@gmail.com>
2026-04-09 21:29:59 +0300

    added a Multiplication method.

--- c#_playground/Functions.cs
+++ c#_playground/Functions.cs
@@ -18,5 +18,17 @@ namespace CSharpPractice.Functions
 
             Console.WriteLine($"The sum of {num1, 2} and {num2, 2} is {num1 + num2, 3}");
         }
+        // Multiplication Function
+        public static void Multiplication()
+        {
+            Console.WriteLine("\n=== Performing Multiplication ===");
+            Console.Write("Enter first number: ");
+            double.TryParse(Console.ReadLine(), out double num1);
+
+            Console.Write("Enter second number: ");
+            double.TryParse(Console.ReadLine(), out double num2);
+
+            Console.WriteLine($"The product of {num1} and {num2} is {num1 * num2:F3}");
+        }
     }
 }
