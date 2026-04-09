d95753efd003637b952a9e901c6a0a88290ec678
Tonny Blair <jusanotherthursday@gmail.com>
2026-04-09 21:21:57 +0300

    added an addition method.

--- c#_playground/Functions.cs
+++ c#_playground/Functions.cs
@@ -6,5 +6,17 @@ namespace CSharpPractice.Functions
         {
             Console.WriteLine("Hello Learner, How's C#(pronounced as C sharp) going?");
         }
+        //Function to add two numbers
+        public static void Addition()
+        {
+            Console.WriteLine("\n=== Performing Addition ===");
+            Console.Write("Enter first number: ");
+            int.TryParse(Console.ReadLine(), out int num1);
+
+            Console.Write("Enter second number: ");
+            int.TryParse(Console.ReadLine(), out int num2);
+
+            Console.WriteLine($"The sum of {num1, 2} and {num2, 2} is {num1 + num2, 3}");
+        }
     }
 }
