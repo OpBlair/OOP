0043735b7a9cf86be60e9081fc45705a6105887a
Tonny Blair <jusanotherthursday@gmail.com>
2026-04-29 21:17:13 +0300

    Added Dictionary

--- c#_playground/Collections.cs
+++ c#_playground/Collections.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 namespace CSharpPractice.Collections
 {
     public class CollectionsLab
@@ -14,6 +15,50 @@ namespace CSharpPractice.Collections
             }
 
             Console.WriteLine("Fruits: " + string.Join(", ", fruits));
+        }
+        // Lists
+        public static void ListsLab()
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Kampala");
+            cities.Add("Paris");
+            cities.Add("Nairobi");
+
+            Console.Write("City \n");
+            foreach( string city in cities)
+            {
+                Console.Write(city + "\n");
+            }
+        }
+
+        // Dictionary
+        public static void DictionaryLab()
+        {
+            Dictionary<string, int> studentAge = new Dictionary<string, int>()
+            {
+                {"Alice", 20},
+                {"Bob", 21},
+                {"Trevor", 18}
+            };
+            studentAge["Charlie"] = 22;
+
+            if (studentAge.ContainsKey("Alice"))
+            {
+                Console.WriteLine($"Alice is {studentAge["Alice"]} years old");
+            }
+
+            if(studentAge.TryGetValue("Bob", out int age))
+            {
+                Console.WriteLine($"Bob is {age}");
+            }
+
+            // Looping through dictionary
+            Console.WriteLine("Student info summary: ");
+            Console.WriteLine($"{"Name", 8} | Age");
+            foreach (var student in studentAge)
+            {
+                Console.WriteLine($"{student.Key, 8} | {student.Value}");
+            }
         }
     }
 }
